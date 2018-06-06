d925c6807a8c16b21587333167ba34deaa7e21de
cbrzn <cesarbrazon10@gmail.com>
2018-06-06 13:59:25 -0400

    fixing buggggs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-    value: string = '0'
+    value:string = '0'
     op:string[] = [] 
     constructor(public navCtrl: NavController) {
     }
@@ -15,36 +15,38 @@ export class HomePage {
   type(e) {
     switch (e) {
       case '+': 
-      if (this.op[0] == null && this.value !== '0') {
-        this.op.push(this.value+'+')
-        this.value = ''
-      } else {
-        this.value = '0'
-        this.op.pop()
-      }
+        if (this.op[0] == null && this.value !== '0') {
+          this.op.push(this.value+'+')
+          this.value = ' '
+        } else {
+          this.value = '0'
+          this.op.pop()
+        }
       break
       case '-': 
-      if (this.op[0] == null && this.value !== '0') {
-        this.op.push(this.value+'-')
-        this.value = ''
-      } else {
-        this.value = '0'
-        this.op.pop()
-      }
+        if (this.op[0] == null && this.value !== '0') {
+          this.op.push(this.value+'-')
+          this.value = ' '
+        } else {
+          this.value = '0'
+          this.op.pop()
+        }
       break
       case '*': 
-      if (this.op[0] == null && this.value !== '0') {
-        this.op.push(this.value+'*')
-        this.value = ''
-      } else {
-        this.value = '0'
-        this.op.pop()
-      }
+        if (this.op[0] == null && this.value !== '0') {
+          this.op.push(this.value+'*')
+          this.value = ' '
+        } else {
+          this.value = '0'
+          this.op.pop()
+        }
       break
       case '/':
         if (this.op[0] == null && this.value !== '0') {
           this.op.push(this.value+'/')
-          this.value = ''
+          this.value = ' '
+        } else if (this.op){ 
+          
         } else {
           this.value = '0'
           this.op.pop()
@@ -61,21 +63,21 @@ export class HomePage {
         this.value = eval(this.value+'*-1')      
       break
       case '.': 
-        if (this.value.includes('.')) {
-          this.value = this.value
-        } else if (this.value = '0') {
-          this.value = '0.'
-        } else {
-          this.value = this.value + '.'
-        }
-
-        break
+         (this.value.includes('.') ? this.value : this.value += '.')   
+      break
       case '=': 
-        this.value = eval(this.op.toString()+this.value)
-        this.op.pop()
+          if (this.op[0] == undefined) {
+            this.value 
+          }
+          else if (this.op[0].includes('/') && this.value == '0') {
+            this.value = 'Error'  
+          } else {
+            this.value = eval(this.op.toString()+this.value)
+            this.op.pop()
+          }
       break
       default:
-        (this.value == '0' ? this.value = ""+e : this.value += e)
+        (this.value == '0' ? this.value = ""+e : this.op[0] == null ? this.value +=""+e : this.value += e)
       break
     }
   }
